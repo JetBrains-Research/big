a2e59bd428529eed2608861f33cdec6c18112384
Sergei Lebedev <superbobry@gmail.com>
2015-06-25 19:55:42 +0300

    Autoreformat in 'RTreeIndexDetails'

--- src/main/java/org/jbb/big/RTreeIndexDetails.java
+++ src/main/java/org/jbb/big/RTreeIndexDetails.java
@@ -18,10 +18,11 @@ public class RTreeIndexDetails {
   final static int nodeHeaderSize = 4;	/* Size of rTree node header. isLeaf,reserved,childCount. */
 
   public static void writeBlocks(final List<bbiChromUsage> usageList, final Path bedFilePath,
-                           final int itemsPerSlot, final bbiBoundsArray bounds[],
-                           final int sectionCount, final boolean doCompress, final SeekableDataOutput writer,
-                           final int resTryCount, final int resScales[], final int resSizes[],
-                           final int bedCount, final short fieldCount)
+                                 final int itemsPerSlot, final bbiBoundsArray bounds[],
+                                 final int sectionCount, final boolean doCompress,
+                                 final SeekableDataOutput writer,
+                                 final int resTryCount, final int resScales[], final int resSizes[],
+                                 final int bedCount, final short fieldCount)
       throws IOException {
 
     int maxBlockSize = 0;
@@ -39,8 +40,9 @@ public class RTreeIndexDetails {
     /* Will keep track of some things that help us determine how much to reduce. */
     final int resEnds[] = new int[resTryCount];
     int resTry;
-    for (resTry = 0; resTry < resTryCount; ++resTry)
+    for (resTry = 0; resTry < resTryCount; ++resTry) {
       resEnds[resTry] = 0;
+    }
 
     /* Will keep track of some things that help us determine how much to reduce. */
     boolean atEnd = false, sameChrom = false;
@@ -74,9 +76,10 @@ public class RTreeIndexDetails {
 
       /* Check conditions that would end block and save block info and advance to next if need be. */
         if (atEnd || !sameChrom || itemIx >= itemsPerSlot) {
-	/* Save stream to file, compressing if need be. */
-          if (stream.size() > maxBlockSize)
+        /* Save stream to file, compressing if need be. */
+          if (stream.size() > maxBlockSize) {
             maxBlockSize = stream.size();
+          }
           if (doCompress) {
             throw new UnsupportedOperationException("Don't use compression =)");
           } else {
@@ -93,15 +96,17 @@ public class RTreeIndexDetails {
           ++sectionIx;
           itemIx = 0;
 
-          if (atEnd)
+          if (atEnd) {
             break;
+          }
         }
 
       /* Advance to next chromosome if need be and get chromosome id. */
         if (!sameChrom) {
           usage = usageIterator.next();
-          for (resTry = 0; resTry < resTryCount; ++resTry)
+          for (resTry = 0; resTry < resTryCount; ++resTry) {
             resEnds[resTry] = 0;
+          }
         }
 
         chromId = usage.id;
@@ -114,8 +119,9 @@ public class RTreeIndexDetails {
         }
     /* Otherwise just update end. */
         {
-          if (endPos < end)
+          if (endPos < end) {
             endPos = end;
+          }
 	/* No need to update startPos since list is sorted. */
         }
 
@@ -135,7 +141,7 @@ public class RTreeIndexDetails {
           final String s = row[lastField];
           stream.writeString(s);
         }
-        stream.writeChar((char)0);
+        stream.writeChar((char) 0);
 
         itemIx += 1;
 
@@ -143,7 +149,7 @@ public class RTreeIndexDetails {
         for (resTry = 0; resTry < resTryCount; ++resTry) {
           int resEnd = resEnds[resTry];
           if (start >= resEnd) {
-            resSizes[resTry] =  1;
+            resSizes[resTry] = 1;
             resEnds[resTry] = resEnd = start + resScales[resTry];
           }
           while (end > resEnd) {
@@ -160,22 +166,20 @@ public class RTreeIndexDetails {
                                              final int resSizes[])
 
 /* Fill in resScales with amount to zoom at each level, and zero out resSizes based
- * on average span. Returns the number of zoom levels we actually will use. */
-  {
+ * on average span. Returns the number of zoom levels we actually will use. */ {
     int resTryCount = bbiMaxZoomLevels, resTry;
     final int resIncrement = bbiResIncrement;
     final int minZoom = 10;
     int res = aveSize;
-    if (res < minZoom)
+    if (res < minZoom) {
       res = minZoom;
-    for (resTry = 0; resTry < resTryCount; ++resTry)
-    {
+    }
+    for (resTry = 0; resTry < resTryCount; ++resTry) {
       resSizes[resTry] = 0;
       resScales[resTry] = res;
       // if aveSize is large, then the initial value of res is large, and we
       // and we cannot do all 10 levels without overflowing res* integers and other related variables.
-      if (res > 1000000000)
-      {
+      if (res > 1000000000) {
         resTryCount = resTry + 1;
         break;
       }
@@ -189,16 +193,18 @@ public class RTreeIndexDetails {
                                                 final long itemCount,
                                                 final long endFileOffset,
                                                 final wrapObject retLevelCount) {
-
-    if (itemCount == 0)
+    if (itemCount == 0) {
       return null;
-    rTree el, list = null, tree;
+    }
 
-/* Make first level above leaf. */
+    rTree el;
+    rTree list = null;
+    rTree tree;
+
+    /* Make first level above leaf. */
     long nextOffset = itemArray[0].offset;
     int oneSize;
-    for (int i=0; i<itemCount; i += oneSize)
-    {
+    for (int i = 0; i < itemCount; i += oneSize) {
 
     /* Allocate element and put on list. */
       el = new rTree(); //lmAllocVar(lm, el);
@@ -217,12 +223,13 @@ public class RTreeIndexDetails {
       oneSize = 1;
 
       int j;
-      for (j=i+1; j<itemCount; ++j) {
+      for (j = i + 1; j < itemCount; ++j) {
         nextOffset = itemArray[j].offset;//(*fetchOffset)(endItem, context);
-        if (nextOffset != el.startFileOffset)
+        if (nextOffset != el.startFileOffset) {
           break;
-        else
+        } else {
           oneSize++;
+        }
       }
       if (j == itemCount) {
         nextOffset = endFileOffset;
@@ -231,47 +238,39 @@ public class RTreeIndexDetails {
       el.endFileOffset = nextOffset;
 
     /* Expand area spanned to include all items in block. */
-      for (j=1; j<oneSize; ++j)
-      {
-        key = itemArray[i+j].range;
-        if (key.chromIx < el.startChromIx)
-        {
+      for (j = 1; j < oneSize; ++j) {
+        key = itemArray[i + j].range;
+        if (key.chromIx < el.startChromIx) {
           el.startChromIx = key.chromIx;
           el.startBase = key.start;
-        }
-        else if (key.chromIx == el.startChromIx)
-        {
-          if (key.start < el.startBase)
+        } else if (key.chromIx == el.startChromIx) {
+          if (key.start < el.startBase) {
             el.startBase = key.start;
+          }
         }
-        if (key.chromIx > el.endChromIx)
-        {
+        if (key.chromIx > el.endChromIx) {
           el.endChromIx = key.chromIx;
           el.endBase = key.end;
-        }
-        else if (key.chromIx == el.endChromIx)
-        {
-          if (key.end > el.endBase)
+        } else if (key.chromIx == el.endChromIx) {
+          if (key.end > el.endBase) {
             el.endBase = key.end;
+          }
         }
       }
     }
     list = slReverse(list);
-    // list = slReverse(list);
 
-/* Now iterate through making more and more condensed versions until have just one. */
+    /* Now iterate through making more and more condensed versions until have just one. */
     int levelCount = 1;
     tree = list;
-    while (tree.next != null || levelCount < 2)
-    {
+    while (tree.next != null || levelCount < 2) {
       list = null;
       int slotsUsed = blockSize;
-      rTree parent = null, next;
-      for (el = tree; el != null; el = next)
-      {
+      rTree parent = null;
+      rTree next;
+      for (el = tree; el != null; el = next) {
         next = el.next;
-        if (slotsUsed >= blockSize)
-        {
+        if (slotsUsed >= blockSize) {
           slotsUsed = 1;
           parent = new rTree(el); //lmCloneMem(lm, el, sizeof(*el));
           parent.children = el;
@@ -280,41 +279,36 @@ public class RTreeIndexDetails {
 
           parent.next = list; //slAddHead(list, parent);
           list = parent;
-        }
-        else
-        {
+        } else {
           ++slotsUsed;
           //slAddHead(parent.children, el);
           el.next = parent.children;
           parent.children = el;
 
           el.parent = parent;
-          if (el.startChromIx < parent.startChromIx)
-          {
+          if (el.startChromIx < parent.startChromIx) {
             parent.startChromIx = el.startChromIx;
             parent.startBase = el.startBase;
-          }
-          else if (el.startChromIx == parent.startChromIx)
-          {
-            if (el.startBase < parent.startBase)
+          } else if (el.startChromIx == parent.startChromIx) {
+            if (el.startBase < parent.startBase) {
               parent.startBase = el.startBase;
+            }
           }
-          if (el.endChromIx > parent.endChromIx)
-          {
+          if (el.endChromIx > parent.endChromIx) {
             parent.endChromIx = el.endChromIx;
             parent.endBase = el.endBase;
-          }
-          else if (el.endChromIx == parent.endChromIx)
-          {
-            if (el.endBase > parent.endBase)
+          } else if (el.endChromIx == parent.endChromIx) {
+            if (el.endBase > parent.endBase) {
               parent.endBase = el.endBase;
+            }
           }
         }
       }
 
       list = slReverse(list);
-      for (el = list; el != null; el = el.next)
+      for (el = list; el != null; el = el.next) {
         el.children = slReverse(el.children);
+      }
       tree = list;
       levelCount += 1;
     }
@@ -322,46 +316,41 @@ public class RTreeIndexDetails {
     return tree;
   }
 
-  private static void calcLevelSizes(final rTree tree, final int levelSizes[], final int level, final int maxLevel)
-/* Recursively count sizes of levels and add to appropriate slots of levelSizes */
-  {
+  private static void calcLevelSizes(final rTree tree, final int levelSizes[], final int level,
+                                     final int maxLevel)
+/* Recursively count sizes of levels and add to appropriate slots of levelSizes */ {
     rTree el;
-    for (el = tree; el != null; el = el.next)
-    {
+    for (el = tree; el != null; el = el.next) {
       levelSizes[level] += 1;
-      if (level < maxLevel)
-        calcLevelSizes(el.children, levelSizes, level+1, maxLevel);
+      if (level < maxLevel) {
+        calcLevelSizes(el.children, levelSizes, level + 1, maxLevel);
+      }
     }
   }
 
   private static int indexNodeSize(final int blockSize)
-/* Return size of an index node. */
-  {
+/* Return size of an index node. */ {
     return nodeHeaderSize + indexSlotSize * blockSize;
   }
 
   private static int leafNodeSize(final int blockSize)
-/* Return size of a leaf node. */
-  {
+/* Return size of a leaf node. */ {
     return nodeHeaderSize + leafSlotSize * blockSize;
   }
 
   /* Count up elements in list. */
-  private static int slCount(final rTree list)
-  {
+  private static int slCount(final rTree list) {
     rTree pt = list;
     int len = 0;
 
-    while (pt != null)
-    {
+    while (pt != null) {
       len += 1;
       pt = pt.next;
     }
     return len;
   }
 
-  private static rTree slReverse(final rTree list)
-  {
+  private static rTree slReverse(final rTree list) {
     rTree newList = null;
 
     rTree cur = list;
@@ -375,27 +364,25 @@ public class RTreeIndexDetails {
   }
 
   private static long rWriteIndexLevel(final int blockSize, final int childNodeSize,
-                                final rTree tree, final int curLevel, final int destLevel,
-                                final long offsetOfFirstChild, final SeekableDataOutput writer)
+                                       final rTree tree, final int curLevel, final int destLevel,
+                                       final long offsetOfFirstChild,
+                                       final SeekableDataOutput writer)
       throws IOException
 /* Recursively write an index level, skipping levels below destLevel,
- * writing out destLevel. */
-  {
+ * writing out destLevel. */ {
     rTree el;
     long offset = offsetOfFirstChild;
-    if (curLevel == destLevel)
-    {
+    if (curLevel == destLevel) {
     /* We've reached the right level, write out a node header */
       final byte reserved = 0;
       final byte isLeaf = 0;
-      final short countOne = (short)slCount(tree.children);
+      final short countOne = (short) slCount(tree.children);
       writer.writeByte(isLeaf);
       writer.writeByte(reserved);
-      writer.writeShort((int)countOne);
+      writer.writeShort((int) countOne);
 
     /* Write out elements of this node. */
-      for (el = tree.children; el != null; el = el.next)
-      {
+      for (el = tree.children; el != null; el = el.next) {
         writer.writeInt(el.startChromIx);
         writer.writeInt(el.startBase);
         writer.writeInt(el.endChromIx);
@@ -406,15 +393,15 @@ public class RTreeIndexDetails {
 
     /* Write out zeroes for empty slots in node. */
       int i;
-      for (i=countOne; i<blockSize; ++i)
+      for (i = countOne; i < blockSize; ++i) {
         writer.writeByte((char) 0, indexSlotSize);
-    }
-    else
-    {
+      }
+    } else {
     /* Otherwise recurse on children. */
-      for (el = tree.children; el != null; el = el.next)
-        offset = rWriteIndexLevel(blockSize, childNodeSize, el, curLevel+1, destLevel,
+      for (el = tree.children; el != null; el = el.next) {
+        offset = rWriteIndexLevel(blockSize, childNodeSize, el, curLevel + 1, destLevel,
                                   offset, writer);
+      }
     }
     return offset;
   }
@@ -423,22 +410,19 @@ public class RTreeIndexDetails {
                                    final int curLevel, final int leafLevel,
                                    final SeekableDataOutput writer)
       throws IOException
-/* Write out leaf-level nodes. */
-  {
-    if (curLevel == leafLevel)
-    {
+/* Write out leaf-level nodes. */ {
+    if (curLevel == leafLevel) {
     /* We've reached the right level, write out a node header. */
       final byte reserved = 0;
       final byte isLeaf = 1;
-      final short countOne = (short)slCount(tree.children);
+      final short countOne = (short) slCount(tree.children);
       writer.writeByte(isLeaf);
       writer.writeByte(reserved);
       writer.writeShort(countOne);
 
     /* Write out elements of this node. */
       rTree el;
-      for (el = tree.children; el != null; el = el.next)
-      {
+      for (el = tree.children; el != null; el = el.next) {
         writer.writeInt(el.startChromIx);
         writer.writeInt(el.startBase);
         writer.writeInt(el.endChromIx);
@@ -450,47 +434,45 @@ public class RTreeIndexDetails {
 
     /* Write out zeroes for empty slots in node. */
       int i;
-      for (i=countOne; i<itemsPerSlot; ++i)
+      for (i = countOne; i < itemsPerSlot; ++i) {
         writer.writeByte((char) 0, indexSlotSize);
-    }
-    else
-    {
+      }
+    } else {
     /* Otherwise recurse on children. */
       rTree el;
-      for (el = tree.children; el != null; el = el.next)
-        rWriteLeaves(itemsPerSlot, el, curLevel+1, leafLevel, writer);
+      for (el = tree.children; el != null; el = el.next) {
+        rWriteLeaves(itemsPerSlot, el, curLevel + 1, leafLevel, writer);
+      }
     }
   }
 
-  private static void writeTreeToOpenFile(final rTree tree, final int blockSize, final int levelCount, final SeekableDataOutput writer)
+  private static void writeTreeToOpenFile(final rTree tree, final int blockSize,
+                                          final int levelCount, final SeekableDataOutput writer)
       throws IOException
 /* Write out tree to a file that is open already - writing out index nodes from
- * highest to lowest level, and then leaf nodes. */
-  {
+ * highest to lowest level, and then leaf nodes. */ {
 /* Calculate sizes of each level. */
     int i;
     final int levelSizes[] = new int[levelCount];
 //    for (i=0; i<levelCount; ++i)
 //      levelSizes[i] = 0;
-    calcLevelSizes(tree, levelSizes, 0, levelCount-1);
+    calcLevelSizes(tree, levelSizes, 0, levelCount - 1);
 
 /* Calc offsets of each level. */
     final long levelOffsets[] = new long[levelCount];
     long offset = writer.tell();
     final long iNodeSize = indexNodeSize(blockSize);
     final long lNodeSize = leafNodeSize(blockSize);
-    for (i = 0; i < levelCount; ++i)
-    {
+    for (i = 0; i < levelCount; ++i) {
       levelOffsets[i] = offset;
       offset += levelSizes[i] * iNodeSize;
     }
 
 /* Write out index levels. */
-    final int finalLevel = levelCount-3;
-    for (i=0; i<=finalLevel; ++i)
-    {
-      final long childNodeSize = (i==finalLevel ? lNodeSize : iNodeSize);
-      rWriteIndexLevel(blockSize, (int)childNodeSize, tree, 0, i, levelOffsets[i+1], writer);
+    final int finalLevel = levelCount - 3;
+    for (i = 0; i <= finalLevel; ++i) {
+      final long childNodeSize = (i == finalLevel ? lNodeSize : iNodeSize);
+      rWriteIndexLevel(blockSize, (int) childNodeSize, tree, 0, i, levelOffsets[i + 1], writer);
     }
 
 /* Write out leaf level. */
@@ -512,8 +494,9 @@ public class RTreeIndexDetails {
     final rTree dummyTree = new rTree();
     dummyTree.startBase = 0; // struct rTree dummyTree = {.startBase=0};
 
-    if (tree == null)
-      tree = new rTree(dummyTree);	// Work for empty files....
+    if (tree == null) {
+      tree = new rTree(dummyTree);        // Work for empty files....
+    }
 
     final int magic = 0x2468ACE0;
     final int reserved = 0;
