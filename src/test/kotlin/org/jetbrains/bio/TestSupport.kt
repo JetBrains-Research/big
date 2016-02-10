bad6826982032925a412e2f3d8972894c9cf7071
Sergei Lebedev <superbobry@gmail.com>
2016-02-10 20:39:39 +0300

    Temporarily disabled inlines in 'SeekableDataIOTest'
      to debug failures on AppVeyor

--- src/test/kotlin/org/jetbrains/bio/TestSupport.kt
+++ src/test/kotlin/org/jetbrains/bio/TestSupport.kt
@@ -36,8 +36,8 @@ internal fun Path.chromosomes(): List<Pair<String, Int>> {
     }.toList()
 }
 
-internal inline fun withTempFile(prefix: String, suffix: String,
-                                 block: (Path) -> Unit) {
+internal fun withTempFile(prefix: String, suffix: String,
+                          block: (Path) -> Unit) {
     val path = Files.createTempFile(prefix, suffix)
     try {
         block(path)
